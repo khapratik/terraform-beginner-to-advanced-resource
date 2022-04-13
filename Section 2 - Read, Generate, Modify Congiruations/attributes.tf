265bc89edfa0a762bbedad7224901e0fde7464df
khapratik <37405566+khapratik@users.noreply.github.com>
2022-04-14 08:45:30 +1000

    Update attributes.tf

--- Section 2 - Read, Generate, Modify Congiruations/attributes.tf
+++ Section 2 - Read, Generate, Modify Congiruations/attributes.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+  access_key = "AKIARYUH47AZ4IBFLTF2"
+  secret_key = "FkAdU0GgRquVO2j+frxk9+uFxvZJhEqfq10SRT/a"
 }
 
 resource "aws_eip" "lb" {
